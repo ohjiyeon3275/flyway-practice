f31c3366fffe68b8020e7ce1500f82ea719dc486
ohjiyeon123 <brother@Ohs-MacBook-Air.local>
2021-10-17 12:03:15 +0900

    main 뜨게 수정

--- src/main/java/com/jiyeon/project/controller/MainController.java
+++ src/main/java/com/jiyeon/project/controller/MainController.java
@@ -1,16 +1,16 @@
 package com.jiyeon.project.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
-@RequestMapping("/")
+@Controller
 public class MainController {
 
     @GetMapping("/main")
     public String main(){
-        return "/templates/main";
+
+        return "main.html";
     }
 }
